3f588a1a615cb0f0e8bc691acf76ff4270f5e1a4
Simon Perrigault <simonperrigault22@gmail.com>
2023-12-31 14:08:35 +0100

    affichage de la liste

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let text = document.querySelector("input[type=\"text\"]");
 let form = document.querySelector("form");
+let liste = document.querySelector("ul");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(text.value);
+  liste.innerHTML += `<li>${text.value}</li>`;
   text.value = "";
 });
