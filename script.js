4863d1bc786a8a16ae9dfc7787c018ead194294a
Simon Perrigault <simonperrigault22@gmail.com>
2023-12-31 14:30:58 +0100

    local storage

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ let text = document.querySelector("input[type=\"text\"]");
 let form = document.querySelector("form");
 let liste = document.querySelector("ul");
 
+let before = JSON.parse(localStorage.getItem("liste"));
+if (before != null) {
+  before.forEach((element) => {
+    liste.innerHTML += `<li>${element}</li>`;
+  });
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   liste.innerHTML += `<li>${text.value}</li>`;
+  let curr = JSON.parse(localStorage.getItem("liste"));
+  if (curr == null) {
+    curr = [];
+  }
+  curr.push(text.value);
+  localStorage.setItem("liste", JSON.stringify(curr));
   text.value = "";
 });
