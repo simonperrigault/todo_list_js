1c667688acaebb5d3d1b41dc10505b8509bcffb6
Simon Perrigault <simonperrigault22@gmail.com>
2023-12-31 14:58:33 +0100

    delete on click

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let text = document.querySelector("input[type=\"text\"]");
 let form = document.querySelector("form");
 let liste = document.querySelector("ul");
+let reset = document.getElementById("reset");
 
-let before = JSON.parse(localStorage.getItem("liste"));
-if (before != null) {
-  before.forEach((element) => {
+let curr = JSON.parse(localStorage.getItem("liste"));
+if (curr != null) {
+  curr.forEach((element) => {
     liste.innerHTML += `<li>${element}</li>`;
   });
+}
+else {
+  curr = [];
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   liste.innerHTML += `<li>${text.value}</li>`;
-  let curr = JSON.parse(localStorage.getItem("liste"));
-  if (curr == null) {
-    curr = [];
-  }
   curr.push(text.value);
   localStorage.setItem("liste", JSON.stringify(curr));
   text.value = "";
+});
+
+reset.addEventListener("click", () => {
+  localStorage.setItem("liste", JSON.stringify([]));
+  curr = [];
+  liste.innerHTML = "";
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    let name = event.target.innerHTML;
+    curr.splice(curr.indexOf(name), 1);
+    liste.removeChild(event.target);
+    localStorage.setItem("liste", JSON.stringify(curr));
+  }
 });
