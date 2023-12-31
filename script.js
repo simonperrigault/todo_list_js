31dcf023064c758714e0608223b599c799417b10
Simon Perrigault <simonperrigault22@gmail.com>
2023-12-31 13:56:05 +0100

    suite

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let submit = document.querySelector("input[type=\"submit\"");
+let text = document.querySelector("input[type=\"text\"]");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(text.value);
+  text.value = "";
+});
